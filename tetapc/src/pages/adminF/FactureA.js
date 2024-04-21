47db4d95a2266742b69e4bb44ac6085c7783d300
MEDALI003 <tmedali7@gmail.com>
2024-04-21 12:25:53 +0100

    admin vision to the facture

---
+++ tetapc/src/pages/adminF/FactureA.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useDispatch ,useSelector } from 'react-redux'
+
+const FactureA = () => {
+    const dispatch=useDispatch()
+    const basket=useSelector(state=>state.basket.basket)
+  return (
+    <div>
+        <div style={{display:"flex",flexDirection:"column"}}>
+            {basket.map(el=><div></div>)}
+        </div>
+    </div>
+  )
+}
+
+export default FactureA
